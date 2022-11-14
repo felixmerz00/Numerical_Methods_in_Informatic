5defdc3893ad7cb7c82918e73875f49fc5c59b49
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-11-14 20:51:19 +0100

    Exercise b solved, exercise a, not solved

--- Exercise3/backend_jan.py
+++ Exercise3/backend_jan.py
@@ -5,16 +5,14 @@ import numpy as np
 # Input: lists sourceBase and targetBase - lists of vectors (e.g. [np.array([1, 2, 3]), np.array([2, 0, 1]), ...])
 # Output: Matrix A - a 2D np.array, with len(sourceBase) x len(targetBase) entries
 def changeBase(sourceBase: list, targetBase: list) -> np.array:
-  # target base = source base setzten
-  # Gleichungssystem in reduced row echelon form bringen
-  # Die Lösung von diesem Gleichungssytem ist die 
-  return np.identity(len(targetBase))
+  inv = np.linalg.inv(targetBase)
+  return np.matmul(inv, np.transpose(sourceBase))
 
 # Task b)
 # Implement a method, checking if a subBase spans a Subvectorspace of the space spanned by the given base
 # Input: lists sourceBase and subSpace - lists of vectors (e.g. [np.array([1, 2, 3]), np.array([2, 0, 1]), ...])
 # Output: bool
 def spansSubSpace(sourceBase: list, subBase: list) -> bool:
+  #if (len(sourceBase[0]) != len(subBase[0])): return False
   u = sourceBase + subBase
-  out = np.linalg.matrix_rank(u) <= np.linalg.matrix_rank(sourceBase)
-  return out
+  return np.linalg.matrix_rank(u) <= np.linalg.matrix_rank(sourceBase)
